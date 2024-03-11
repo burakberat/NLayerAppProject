b073128f2c156f0c675c558ee87f3605e68b2e8d
Burak Berat Orucoglu <burakorucoglu@outlook.com>
2024-03-11 07:32:10 +0300

    Add project files.

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace NLayer.Core
+{
+    public abstract class BaseEntity //new'lenemez.
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
