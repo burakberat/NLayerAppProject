cc33ca5ec2cb8fa20f113dd344ebb995247ec933
Burak Berat Orucoglu <burakorucoglu@outlook.com>
2024-05-20 20:56:21 +0300

    Configuraiton işlemleri yapıldı.

--- NLayer.Core/Entities/ProductFeature.cs
+++ NLayer.Core/Entities/ProductFeature.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Color { get; set; }
         public int Height { get; set; }
-        public int width { get; set; }
+        public int Width { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
     }
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Entities;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id = 1, CategoryId = 1, Name = "Rotring", Price = 100, Stock = 20, CreatedDate = DateTime.Now },
+                new Product { Id = 2, CategoryId = 1, Name = "Faber Castle", Price = 200, Stock = 30, CreatedDate = DateTime.Now },
+                new Product { Id = 3, CategoryId = 1, Name = "Tuna", Price = 50, Stock = 50, CreatedDate = DateTime.Now },
+                new Product { Id = 4, CategoryId = 2, Name = "Sefiller", Price = 100, Stock = 20, CreatedDate = DateTime.Now },
+                new Product { Id = 5, CategoryId = 2, Name = "Kumarbaz", Price = 100, Stock = 20, CreatedDate = DateTime.Now });
+        }
+    }
+}
